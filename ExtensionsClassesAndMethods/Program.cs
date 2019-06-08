ded7a45f3d18418a5dbc04f6031cd1ba98397053
billytukija <billytukija@gmail.com>
2019-06-08 03:24:13 -0300

    Initial commit

---
+++ ExtensionsClassesAndMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionsClassesAndMethods
+{
+    public static class Extensions {
+
+        // The method "WriteJSON" gonna be injected in "strings"
+        public static void WriteJSON(this string str,string fileName) {
+            // Logic
+        }
+
+        public static void BuildUrl(this string str) { }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            s.WriteJSON("");
+
+            s.BuildUrl();
+        }
+    }
+}
